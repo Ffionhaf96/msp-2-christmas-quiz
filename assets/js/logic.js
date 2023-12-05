fc49c76bd26d9bdd5bc6cd7e5c2dfdf5db69a483
Ffion Haf <ffionhaf33@gmail.com>
2023-12-05 19:01:12 +0000

    wip

---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+// What am I doing here 
+
+// 1. Create a function that will start the quiz
+// 2. Create a function that will end the quiz
+// 3. Create a function that will check the answers
+// 4. Create a function that will display the results
+// 5. Create a function that will display the questions
+// 6. Create a function that will display the timer
+// 7. Create a function that will display the high scores
+// 8. Create a function that will clear the high scores
+// 9. Create a function that will store the high scores
+// 10. Create a function that will display the high scores
+
+
+// Path: msp-2-christmas-quiz/assets/js/script.js
