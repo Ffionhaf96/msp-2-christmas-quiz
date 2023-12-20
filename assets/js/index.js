87659da3fccacdaf421bc571251c6cb374bb24c6
Ffion Haf <ffionhaf33@gmail.com>
2023-12-20 19:17:03 +0000

    feat: avatar selector

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,26 @@ function handleSaveName() {
 
 const saveName = document.querySelector('.save-name');
 saveName.addEventListener('click', handleSaveName);
+
+const avatarItems = document.querySelectorAll('.avatar-selector ul li');
+console.log(avatarItems);
+
+function handleSelectAvatar(event) {
+    // Remove 'selected' class from all list item
+    avatarItems.forEach(li => li.classList.remove('selected-avatar'));
+    // Add 'selected' class to the clicked item
+    event.target.classList.add('selected-avatar');
+    // Save the value of the avatar
+    const clickedAvatarIndex = Array.from(avatarItems).indexOf(event.target);
+
+    const chosenAvatar = Array.from(avatarItems)[clickedAvatarIndex].textContent;
+    localStorage.setItem('avatar', chosenAvatar);
+    document.querySelector('.avatar-prompt').textContent = "Your current avatar!";
+    document.querySelector('.chosen-avatar').textContent = localStorage.getItem('avatar');
+}
+
+avatarItems.forEach((li, index) => {
+    li.addEventListener('click', handleSelectAvatar);
+});
+
+const startGame = document.querySelector('.start-game');
