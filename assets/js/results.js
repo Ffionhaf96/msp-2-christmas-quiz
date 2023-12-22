b03d43a1ada5d4922553283e2b3412b267bc8e08
Ffion Haf <ffionhaf33@gmail.com>
2023-12-22 16:00:41 +0000

    feat: show christmas category result based on score

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,39 +1,78 @@
+async function getQuestionData() {
+    try {
+        const response = await fetch('assets/questions/questions.json');
+        if (!response.ok) {
+            // We couldn't get the quiz JSON
+            console.log(`HTTP error! Status: ${response.status}`);
+        }
+        // wait for response and access JSON from file
+        return await response.json();
+    } catch (error) {
+        console.log(`Couldn't fetch the quiz data - ${error}`);
+        return error;
+    }
+}
+
 function restartQuiz() {
     localStorage.clear();
-    window.location.href="index.html"
+    window.location.href = "index.html"
 }
 
 const restartQuizButton = document.querySelector('#restart');
 //add event listener to restart button
 restartQuizButton.addEventListener('click', restartQuiz)
 
-const nickname = document.querySelector('.nickname');
-nickname.textContent =localStorage.getItem('nickname');
+const teaser = document.querySelector('.teaser');
+teaser.textContent = `How well do you know Christmas - ${localStorage.getItem('nickname')}?!`;
 
 
 async function calculateResults() {
     // initialise score
     let score = 0;
-    try {
-        const response = await fetch('assets/questions/questions.json');
-        if (!response.ok) {
-            // We couldn't get the quiz JSON
-            throw new Error(`HTTP error! Status: ${response.status}`);
+    const chosenAnswers = JSON.parse(localStorage.getItem('chosenAnswers'));
+
+    const questionData = await getQuestionData();
+    const questions = questionData.questions;
+    console.log(questions);
+    // loop over each question and check if answers match
+    questions.forEach((quest, index) => {
+        if (quest.answer == chosenAnswers[index]) {
+            ++score;
         }
-        // wait for response and access JSON from file
-        const data = await response.json();
-        // return the quiz data
-        const chosenAnswers = JSON.parse(localStorage.getItem('chosenAnswers'));
-        
-       data.questions.forEach((quest, index) => {
-            if (quest.answer == chosenAnswers[index]) {
-                ++score;
-            }
-       });
-       document.querySelector('#score').textContent = `Wow you scored ${score} / 5`;
-    } catch (error) {
-        console.error('Fetch error:', error);
-    }
+    });
+
+    const questionLength = questions.length;
+    // calculate score percentage
+    const scorePercentage = (score / questionLength) * 100;
+    const resultCategory = await getResultCategory(scorePercentage);
+    console.log(resultCategory);
+    // set all the result category properties
+    document.querySelector('.title').textContent = `You are a ${resultCategory.title}`;
+    document.querySelector('.description').textContent = resultCategory.description;
+    document.querySelector('.category-image').src = `../assets/images/${resultCategory.image}`;
+    document.querySelector('.suggestion').textContent = resultCategory.suggestion;
+    // show score in score element
+    document.querySelector('#score').textContent = `You scored ${score} / ${questionLength}`;
 }
 
+async function getResultCategory(scorePercentage) {
+    const questionData = await getQuestionData();
+    const resultCategories = questionData.resultCategories;
+    console.log(resultCategories);
+    let index = 0;
+    if (scorePercentage == 0){
+        return resultCategories[index];
+    }
+    else if (scorePercentage <= 20) {
+        return resultCategories[++index];
+    } else if (scorePercentage <= 40) {
+        return resultCategories[++index];
+    } else if (scorePercentage <= 60) {
+        return resultCategories[++index];
+    } else if (scorePercentage <= 80) {
+        return resultCategories[++index];
+    } else if (scorePercentage <= 100) {
+        return resultCategories[++index];
+    }
+}
 calculateResults();
