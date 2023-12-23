10c6f513cde4840a0cb3632ccbb0ed3a229e6fb4
Ffion Haf <ffionhaf33@gmail.com>
2023-12-23 11:38:39 +0000

    fix(a11y): target heading element

--- assets/js/game.js
+++ assets/js/game.js
@@ -58,7 +58,7 @@ async function showQuestion(index) {
     console.debug(question);
 
     // get the elements to replace data
-    const questionSection = document.getElementById('question');
+    const questionSection = document.querySelector('#question h2');
 
     // change the textContent with the question
     questionSection.textContent = question.question;
